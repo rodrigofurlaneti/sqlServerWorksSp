9707f077b15652f39dbdb5ea4b494048809d9534
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-02-21 15:33:14 -0300

    Create GetByName.sql

---
+++ Procedures/User/GetByName.sql
@@ -0,0 +1,17 @@
+USE [WorksSp]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[USP_WorksSp_User_GetByName] (@UserName VARCHAR(50))
+AS
+BEGIN
+	SELECT TOP 1 * FROM [dbo].[WorksSp_User]
+		WHERE UserName = @UserName;
+	SET NOCOUNT ON;
+END
+GO

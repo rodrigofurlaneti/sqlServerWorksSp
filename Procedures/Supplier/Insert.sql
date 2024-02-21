d1cb911c45a9b1ae56fd7ffb58bd102758968bdd
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-02-21 17:52:49 -0300

    Create Insert.sql

---
+++ Procedures/Supplier/Insert.sql
@@ -0,0 +1,29 @@
+USE [WorksSp]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[USP_WorksSp_Supplier_Insert] (@SupplierName VARCHAR(50),
+												@DateInsert DATETIME,
+												@DateUpdate DATETIME,
+												@UserId INT,
+												@UserName VARCHAR(150))
+AS
+BEGIN
+	INSERT INTO [dbo].[WorksSp_Supplier] (SupplierName,
+						 DateInsert,
+						 DateUpdate,
+						 UserId,
+						 UserName)
+	VALUES (@SupplierName,
+			@DateInsert,
+			@DateUpdate,
+			@UserId,
+			@UserName)
+	SET NOCOUNT ON;
+END
+GO

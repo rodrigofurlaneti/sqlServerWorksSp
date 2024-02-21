4bdf8f808c56fc9ec9d9d188738321a54faae8bc
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-02-21 17:43:22 -0300

    Create GetAll.sql

---
+++ Procedures/Color/GetAll.sql
@@ -0,0 +1,18 @@
+USE [WorksSp]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[USP_WorksSp_Color_GetAll] 
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT *
+		FROM [dbo].[WorksSp_Color]
+			ORDER BY ColorName ASC;
+END
+GO

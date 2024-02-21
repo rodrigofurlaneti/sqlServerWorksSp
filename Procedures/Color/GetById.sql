bcc2fdaeaa97efeb1ee5f7be1253db69a4f7652e
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-02-21 17:41:03 -0300

    Create GetById.sql

---
+++ Procedures/Color/GetById.sql
@@ -0,0 +1,17 @@
+USE [WorksSp]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[USP_WorksSp_Color_GetById] (@ColorId INT)
+AS
+BEGIN
+	SELECT TOP 1 * FROM [dbo].[WorksSp_Color]
+		WHERE ColorId = @ColorId;
+	SET NOCOUNT ON;
+END
+GO

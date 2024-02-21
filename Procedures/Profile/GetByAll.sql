086d8dbaa63c7ec5a3ef4efef7f86e63e9fc92e7
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-02-21 16:57:09 -0300

    Create GetByAll.sql

---
+++ Procedures/Profile/GetByAll.sql
@@ -0,0 +1,18 @@
+USE [WorksSp]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[USP_WorksSp_Profile_GetAll] 
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT *
+		FROM [dbo].[WorksSp_Profile]
+			ORDER BY ProfileName ASC;
+END
+GO

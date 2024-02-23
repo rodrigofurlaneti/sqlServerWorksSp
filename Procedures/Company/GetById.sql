53ace80fdf043c5d17e6da0f13791ec1f18ed87d
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2024-02-23 11:43:23 -0300

    Create GetById.sql

---
+++ Procedures/Company/GetById.sql
@@ -0,0 +1,17 @@
+USE [WorksSp]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[USP_WorksSp_Company_GetById] (@CompanyId INT)
+AS
+BEGIN
+	SELECT TOP 1 * FROM [dbo].[WorksSp_Company]
+		WHERE CompanyId = @CompanyId;
+	SET NOCOUNT ON;
+END
+GO
